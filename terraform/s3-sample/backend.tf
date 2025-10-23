42e886d9e432e466ecffe7a0eee6d337d3a3ee1b
Peixuan.liu <Peixuan.liu@jtp.co.jp>
2025-10-23 21:24:12 +0900

    Add S3 backend configuration for terraform state

--- terraform/s3-sample/backend.tf
+++ terraform/s3-sample/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-ims-test-bucket-20251023"
-    key            = "terraform/s3-sample/${terraform.workspace}/terraform.tfstate"
+    key            = "terraform/s3-sample/terraform.tfstate"
     region         = "ap-northeast-1"
     encrypt        = true
   }
