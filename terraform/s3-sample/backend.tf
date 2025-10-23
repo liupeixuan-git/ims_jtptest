6abbe25678abf8f71f05181206b375894df2e38a
Peixuan.liu <Peixuan.liu@jtp.co.jp>
2025-10-23 21:51:17 +0900

    Fix backend.tf formatting and set region to ap-northeast-1

--- terraform/s3-sample/backend.tf
+++ terraform/s3-sample/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ims-test-bucket-20251023"
-    key            = "terraform/s3-sample/terraform.tfstate"
-    region         = "ap-northeast-1"
-    encrypt        = true
+    bucket  = "my-ims-test-bucket-20251023"
+    key     = "terraform/s3-sample/terraform.tfstate"
+    region  = "ap-northeast-1"
+    encrypt = true
   }
 }
